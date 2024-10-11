5b283a8ace30014a5e056b884c7b891aae2ff4ce
Thai Bao Duy <thaibaoduy.5724@gmail.com>
2024-10-11 21:47:59 +0700

    part35

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -6,19 +6,21 @@ const ProductDetailsPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   return (
-    <div
-      style={{ padding: "0 120px", background: "#efefef", height: "1000px" }}
-    >
-      <h4>
-        <span
-          style={{ cursor: "pointer", fontWeight: "bold" }}
-          onClick={() => navigate("/")}
-        >
-          Home
-        </span>
-        - Product details
-      </h4>
-      <ProductDetailsComponent idProduct={id}></ProductDetailsComponent>
+    <div style={{ height: "100vh", width: "100%", background: "#efefef" }}>
+      <div
+        style={{ padding: "0 120px", background: "#efefef", height: "1000px" }}
+      >
+        <h4>
+          <span
+            style={{ cursor: "pointer", fontWeight: "bold" }}
+            onClick={() => navigate("/")}
+          >
+            Home
+          </span>
+          - Product details
+        </h4>
+        <ProductDetailsComponent idProduct={id}></ProductDetailsComponent>
+      </div>
     </div>
   );
 };
--- src/pages/TypeProductPage/TypeProductPage.jsx
+++ src/pages/TypeProductPage/TypeProductPage.jsx
@@ -1,43 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarComponent from "../../component/NavbarComponent/NavbarComponent";
 import CardComponent from "../../component/CardComponent/CardComponent";
 import { Row, Col, Pagination } from "antd";
 import { WrapperNavbar, WrapperProduct } from "./style";
+import { useLocation } from "react-router-dom";
+import * as ProductService from "../../services/ProductService";
+import Loading from "../../component/LoadingComponent/Loading";
 
 const TypeProductPage = () => {
+  const { state } = useLocation();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProductType = async (type) => {
+    setLoading(true);
+    const res = await ProductService.getProductType(type);
+    if (res?.status === "OK") {
+      setLoading(false);
+      setProducts(res?.data);
+      console.log("res", res);
+    } else {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (state) {
+      fetchProductType(state);
+    }
+  }, [state]);
+
   const onChange = () => {};
   return (
-    <div style={{ width: "100%", background: "#efefef" }}>
-      <div style={{ width: "1270px", margin: "0 auto" }}>
-        <Row
-          style={{
-            flexWrap: "nowrap",
-            paddingTop: "10px",
-          }}
-        >
-          <WrapperNavbar span={4}>
-            <NavbarComponent></NavbarComponent>
-          </WrapperNavbar>
-          <Col span={20}>
-            <WrapperProduct>
-              <CardComponent></CardComponent>
-              <CardComponent></CardComponent>
-              <CardComponent></CardComponent>
-              <CardComponent></CardComponent>
-              <CardComponent></CardComponent>
-              <CardComponent></CardComponent>
-              <CardComponent></CardComponent>
-            </WrapperProduct>
-            <Pagination
-              defaultCurrent={2}
-              total={100}
-              onChange={onChange}
-              style={{ justifyContent: "center", marginTop: "15px" }}
-            />
-          </Col>
-        </Row>
+    <Loading isPending={loading}>
+      <div
+        style={{
+          width: "100%",
+          background: "#efefef",
+          height: "calc(150vh - 65px)",
+        }}
+      >
+        <div style={{ width: "1270px", margin: "0 auto" }}>
+          <Row
+            style={{
+              flexWrap: "nowrap",
+              paddingTop: "10px",
+            }}
+          >
+            <WrapperNavbar span={4}>
+              <NavbarComponent></NavbarComponent>
+            </WrapperNavbar>
+            <Col
+              span={20}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <WrapperProduct>
+                {products?.map((product) => {
+                  return (
+                    <CardComponent
+                      key={product._id}
+                      countInStock={product.countInStock}
+                      description={product.description}
+                      image={product.image}
+                      name={product.name}
+                      price={product.price}
+                      rating={product.rating}
+                      type={product.type}
+                      discount={product.discount}
+                      selled={product.selled}
+                      id={product._id}
+                    ></CardComponent>
+                  );
+                })}
+              </WrapperProduct>
+              <Pagination
+                defaultCurrent={2}
+                total={100}
+                onChange={onChange}
+                style={{ justifyContent: "center", marginTop: "15px" }}
+              />
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
+    </Loading>
   );
 };
 
